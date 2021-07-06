2718fe60dcf9c14d91e5a9f3b013cae1954119e9
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2021-07-06 13:06:46 +0700

    update

--- web/models.py
+++ web/models.py
@@ -1,31 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-CAT_CHOICES = [
-    (1, 'News'),
-    (2, 'Advertise'),
-    (3, 'Blog')
-]
-
-
-class Blog(models.Model):
-    """Model definition for Blog."""
-
-    # TODO: Define fields here
-    title = models.CharField(max_length=250)
-    content = models.TextField()
-    category = models.IntegerField(choices=CAT_CHOICES)
-    date_created = models.DateTimeField(auto_created=True)
-    date_updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        """Meta definition for Blog."""
-
-        verbose_name = 'Blog'
-        verbose_name_plural = 'Blogs'
-
-    def __str__(self):
-        """Unicode representation of Blog."""
-        pass
